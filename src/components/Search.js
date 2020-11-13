d533d1fdc81d9d8e47763b5463802105649e1a31
oguzhanzny <oguzhanzny@gmail.com>
2020-11-13 08:17:15 +0300

    first commit

---
+++ src/components/Search.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+export class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {
+      keyword: '',
+    };
+  }
+  onChange(e) {
+    this.setState({
+      keyword: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    if (this.state.keyword === '') {
+      this.props.setAlert('Aratılıcak bir Kelime Gir', 'danger');
+    } else {
+      this.props.searchUsers(this.state.keyword);
+      this.setState({ keyword: '' });
+    }
+  }
+
+  render() {
+    return (
+      <div className="container my-3">
+        <form onSubmit={this.onSubmit}>
+          <div className="input-group">
+            <input
+              type="text"
+              value={this.state.keyword}
+              onChange={this.onChange}
+              className="form-control"
+            />
+            <div className="input-group-append">
+              <button type="submit" className="btn btn-primary">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+        {this.props.showClearButton && (
+          <button
+            className="btn btn-secondary btn-sm btn-block"
+            onClick={this.props.clearUser}
+          >
+            Clear Results
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Search;
