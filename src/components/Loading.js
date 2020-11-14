8f3cf0a62cfb6bca1563e3d21cc68a5806552c81
oguzhanzny <oguzhanzny@gmail.com>
2020-11-14 05:07:23 +0300

    react-router-dom and UserDetails added

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import loading from './loading.gif';
+import React from 'react'
+import loading from './loading.gif'
 
-export const Loading = () => {
-  return (
-    <React.Fragment>
-      <img
-        src={loading}
-        alt="loading..."
-        style={{ width: '200px', display: 'block', margin: 'auto' }}
-      />
-    </React.Fragment>
-  );
-};
-export default Loading;
+const Loading = () => {
+    return (
+        <React.Fragment>
+            <img src={`/${loading}`}  alt="Loading..." style={{width: '200px', display: 'block', margin: 'auto'}}/>
+        </React.Fragment>
+    )
+}
+export  default Loading;
